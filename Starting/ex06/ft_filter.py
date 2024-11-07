ebece090c43eb03689ab45d938028672c7beafff
fZpHr <afterrush@live.fr>
2024-11-07 14:49:57 +0100

    Fix: None issue on ex06

--- Starting/ex06/ft_filter.py
+++ Starting/ex06/ft_filter.py
@@ -3,21 +3,29 @@ def is_even(n):
 
 
 def ft_filter(function, iterable):
-    for item in iterable:
-        if function(item):
-            yield item
+    if function is None:
+        for item in iterable:
+            if item:
+                yield item
+    else:
+        for item in iterable:
+            if function(item):
+                yield item
 
 
 def main():
     nb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    list_true_false = [True, False, True, False, True, False, True, False]
+    list_true2_false = [True, False, True, False, True, False, True, False]
     nb2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     next_nb = ft_filter(is_even, nb)
     print(list(next_nb))
-    print(next_nb)
-
-    text = filter(is_even, nb2)
+    text = ft_filter(is_even, nb2)
     print(list(text))
-    print(text)
+    next_nb = ft_filter(None, list_true_false)
+    print(list(next_nb))
+    text = ft_filter(None, list_true2_false)
+    print(list(text))
 
 
 if __name__ == "__main__":
